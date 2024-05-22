1076ece45c4d3016ecc2ee2683b096cda08ede91
scythe7git <joachimgcontreras@gmail.com>
2024-05-23 11:27:23 +1200

    Create initial project.

---
+++ Minesweeper/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * This class is where the whole game runs.
+ *
+ * Author: Joa Contreras
+ * Version: v1.0
+ */
+public class Main
+{
+
+    
+    
+    public Main()
+    {
+        
+    }
+}
