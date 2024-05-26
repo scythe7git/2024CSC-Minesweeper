80a12d2cc764f9adf85ccaf0a520aa7e6e034aef
scythe7git <joachimgcontreras@gmail.com>
2024-05-27 10:01:00 +1200

    Create new project, renamed to Minesweeper

---
+++ Minesweeper/Minesweeper.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Minesweeper here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Minesweeper
+{
+    
+    public Minesweeper()
+    {
+        System.out.println("Hello world!");
+    }
+}
